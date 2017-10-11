56f4e45e779f70f1487c452c4952e9a808fe8616
Gabriel Kazuo <kazuo.gabriel@gmail.com>
2017-10-10 22:08:03 -0300

    Aula01

--- app/src/main/java/br/com/alura/agenda/ListaAlunosActivity.java
+++ app/src/main/java/br/com/alura/agenda/ListaAlunosActivity.java
@@ -2,6 +2,8 @@ package br.com.alura.agenda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ListaAlunosActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class ListaAlunosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
+
+        //Referência para as Views
+        ListView listaAlunos = (ListView) findViewById(R.id.lista_alunos);
+
+        //Mock da lista de alunos
+        String[] alunos = {"Daniel", "Rafael", "Gabriel", "Gustavo", "Alan"};
+
+        //criação e associação do adapter que vai converter String para View
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos);
+        listaAlunos.setAdapter(adapter);
+
+
     }
 }
