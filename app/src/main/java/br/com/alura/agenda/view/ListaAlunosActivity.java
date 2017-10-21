30f7105c91cc7030ae379676d392be02213e4f8d
Gabriel Kazuo <kazuo.gabriel@gmail.com>
2017-10-21 17:56:42 -0200

    S02E01

--- app/src/main/java/br/com/alura/agenda/view/ListaAlunosActivity.java
+++ app/src/main/java/br/com/alura/agenda/view/ListaAlunosActivity.java
@@ -1,6 +1,10 @@
 package br.com.alura.agenda.view;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ContextMenu;
@@ -13,6 +17,7 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.List;
+import java.util.jar.Manifest;
 
 import br.com.alura.agenda.R;
 import br.com.alura.agenda.model.dao.AlunoDAO;
@@ -30,20 +35,6 @@ public class ListaAlunosActivity extends AppCompatActivity {
         //Referência para as Views
         Button btNovoAluno = (Button) findViewById(R.id.btNovoAluno);
         listaAlunos = (ListView) findViewById(R.id.lista_alunos);
-
-        //TUDO DAQUI FOI PRO onResume()
-        //Mock da lista de alunos
-        //String[] alunos = {"Rafael", "Gabriel", "Gustavo", "Alan"};
-
-        //busca a lista de alunos do banco
-        //AlunoDAO dao = new AlunoDAO(this);
-        //List<Aluno> alunos = dao.buscaAlunos();
-        //dao.close();
-
-        //criação e associação do adapter que vai converter String/Aluno para View
-        //quando utilizando mock, trocar o Adapter para o tipo String e não Aluno
-        //ArrayAdapter<Aluno> adapter = new ArrayAdapter<Aluno>(this, android.R.layout.simple_list_item_1, alunos);
-        //listaAlunos.setAdapter(adapter);
 
         listaAlunos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -76,15 +67,54 @@ public class ListaAlunosActivity extends AppCompatActivity {
 
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, final ContextMenu.ContextMenuInfo menuInfo) {
-        //criação do menu deletar e comportamento do clique
-        MenuItem deletar = menu.add(R.string.deletar);
-        deletar.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+        //Recupera posição do aluno clicado
+        final AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+        final Aluno aluno = (Aluno) listaAlunos.getItemAtPosition(info.position);
+
+        MenuItem menuSite = menu.add(R.string.visitar_site);
+        Intent intentSite = new Intent(Intent.ACTION_VIEW);
+        MenuItem menuSMS = menu.add(R.string.sms);
+        Intent intentSMS = new Intent(Intent.ACTION_VIEW);
+        MenuItem menuMapa = menu.add(R.string.maps);
+        Intent intentMapa = new Intent(Intent.ACTION_VIEW);
+        final MenuItem menuLigar = menu.add(R.string.ligar);
+        MenuItem menuDeletar = menu.add(R.string.deletar);
+
+        //Comportamento menu Visitar Site
+        String site = aluno.getSite();
+        if(!site.startsWith("http://")){
+            site = "http://" + site;
+        }
+        intentSite.setData(Uri.parse(site));
+        menuSite.setIntent(intentSite);
+
+        //Comportamento menu SMS
+        intentSMS.setData(Uri.parse("sms:" + aluno.getTelefone()));
+        menuSMS.setIntent(intentSMS);
+
+        //Comportamento Mapa
+        intentMapa.setData(Uri.parse("geo:0,0?q=" + aluno.getEndereco()));
+        menuMapa.setIntent(intentMapa);
+
+        //Comportamento Ligar
+        menuLigar.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
-                //Recupera posição do aluno clicado
-                final AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
-                Aluno aluno = (Aluno) listaAlunos.getItemAtPosition(info.position);
+                if(ActivityCompat.checkSelfPermission(ListaAlunosActivity.this, android.Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(ListaAlunosActivity.this, new String[]{android.Manifest.permission.CALL_PHONE}, 123);
+                } else {
+                    Intent intentLigar = new Intent(Intent.ACTION_CALL);
+                    intentLigar.setData(Uri.parse("tel:" + aluno.getTelefone()));
+                    menuLigar.setIntent(intentLigar);
+                }
+                return false;
+            }
+        });
 
+        //Comportamento Deletar
+        menuDeletar.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
                 Toast.makeText(ListaAlunosActivity.this, "Deletando o aluno " + aluno.getNome(), Toast.LENGTH_SHORT).show();
                 AlunoDAO dao = new AlunoDAO(ListaAlunosActivity.this);
                 dao.deletar(aluno);
